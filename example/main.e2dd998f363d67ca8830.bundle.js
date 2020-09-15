b90f362215b300ab91973e4ea9ac20194f28a057
andrewKz <710468464@qq.com>
2020-09-15 18:01:59 +0800

    change CI travis.yml

---
+++ example/main.e2dd998f363d67ca8830.bundle.js
@@ -0,0 +1,15 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+	[0],
+	{
+		1281: function (n, o, c) {
+			'use strict';
+			c.r(o);
+			c(1282), c(1287);
+		},
+		481: function (n, o, c) {
+			c(482), c(629), c(1281), c(1394), (n.exports = c(1424));
+		},
+		546: function (n, o) {},
+	},
+	[[481, 1, 2]],
+]);
--- storybook-static/main.1bc7966bf37375adf587.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1279:function(n,o,c){"use strict";c.r(o);c(1280)},479:function(n,o,c){c(480),c(627),c(1279),c(1387),n.exports=c(1417)},544:function(n,o){}},[[479,1,2]]]);
