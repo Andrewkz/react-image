72d097dfe76a6a6e7ac3f6e88fffe1152e1df93e
andrewKz <710468464@qq.com>
2020-10-22 14:49:27 +0800

    update tsconfig

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,13 +1,19 @@
-// import React from "react";
+// declare namespace JSX {
+// 	interface IntrinsicElements {
+// 		img: {
+// 			'data-src': string;
+// 			name?: string;
+// 		} & React.DetailedHTMLProps<
+// 			React.ImgHTMLAttributes<HTMLImageElement>,
+// 			HTMLImageElement
+// 		>;
+// 	}
+// }
+import 'react';
 
-declare namespace JSX {
-	interface IntrinsicElements {
-		img: {
-			'data-src': string;
-			name?: string;
-		} & React.DetailedHTMLProps<
-			React.ImgHTMLAttributes<HTMLImageElement>,
-			HTMLImageElement
-		>;
+declare module 'react' {
+	interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
+		'data-src'?: string;
+		name?: string;
 	}
 }
